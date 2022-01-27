975ba3cdb0d56153e916178f29b7e9b3fe070f94
Csaba Soós <csabi.soos.05@gmail.com>
2022-01-27 13:49:13 +0100

    beolvasas, class

---
+++ haromezerje.py
@@ -0,0 +1,24 @@
+class Tanulo:
+    lista = []
+    def __init__(self, tanulokod, nev, micsop, acsop, mnyelv, nem, egyuttlakok, testverszam):
+        self.tanulokod = tanulokod
+        self.nev = nev
+        self.micsop = micsop
+        self.acsop = acsop
+        self.mnyelv = mnyelv
+        self.nem = nem
+        self.egyuttlakok = egyuttlakok
+        self.testverszama = testverszam
+        Tanulo.lista.append(self)
+        
+
+with open("input.txt", "r", encoding="utf8") as f:
+    for sor in f:
+        s = sor.strip().split(";")
+        t = Tanulo(int(s[0]), s[1], s[2], s[3], s[4], s[5], int(s[6]), int(s[7]))
+
+"""for t in Tanulo.lista:
+    print(t.nev)
+
+print(len(Tanulo.lista))
+"""
