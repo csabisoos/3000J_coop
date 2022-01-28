8ee49712c01cc0a6d72d1bb6dfc3bcae429246ce
Balogh Vince <baloghvince.bv@gmail.com>
2022-01-28 11:29:52 +0100

    3. feladat kész

--- paratlanok.py
+++ paratlanok.py
@@ -2,3 +2,12 @@
 
 def feladat_1(lista):
     return len(lista)
+
+# 3) Hány lány tanul az osztályban?
+
+def feladat_3(lista):
+    i = 0
+    for d in lista:
+        if d.nem == "lány":
+            i += 1
+    return i
