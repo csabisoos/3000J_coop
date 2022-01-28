5948c6ba3cd809e34d0f9a04a7bd5ffb23d3d9ac
Csaba Soós <csabi.soos.05@gmail.com>
2022-01-28 12:17:03 +0100

    tökéletesítés

--- parosak.py
+++ parosak.py
@@ -1,8 +1,8 @@
 """paros szamu feladatok megoldasai"""
 def feladat_2(lista):
     fiuk = 0
-    for tanulo in lista:
-        if tanulo.nem == "F":
+    for elem in lista:
+        if elem.nem == "F":
             fiuk += 1
     return fiuk
 
