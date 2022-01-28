05596c4f07ee979009989581052c20874625e9cf
Balogh Vince <baloghvince.bv@gmail.com>
2022-01-28 11:35:32 +0100

    5. feladat kész

--- paratlanok.py
+++ paratlanok.py
@@ -1,13 +1,21 @@
 #1) Hány diák tanul az osztályban?
-
 def feladat_1(lista):
     return len(lista)
 
 # 3) Hány lány tanul az osztályban?
 
 def feladat_3(lista):
-    i = 0
+    result = 0
     for d in lista:
         if d.nem == "lány":
-            i += 1
-    return i
+            result += 1
+    return result
+
+# 5) Gyűjtse ki azon diákok nevét, akiknek több mint 1 testvérük van!
+
+def feladat_5(lista):
+    result = []
+    for d in lista:
+        if d.testverszama < 1:
+            result.append(d.nev)
+    return result
